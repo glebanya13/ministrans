641977edac976fb09f05ce6a42a2356d1df6c179
glebanya13 <65718268+glebanya13@users.noreply.github.com>
2020-10-23 22:02:21 +0300

    config firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,21 +1,21 @@
-export default {
-    apiKey: "AIzaSyDkgawjQv37q19tCMUcch9_76EjiRYsMok",
-    authDomain: "ministrans-60ff9.firebaseapp.com",
-    databaseURL: "https://ministrans-60ff9.firebaseio.com",
-    projectId: "ministrans-60ff9",
-    storageBucket: "ministrans-60ff9.appspot.com",
-    messagingSenderId: "358919010104",
-    appId: "1:358919010104:web:fd0c57f712301f910473cd",
-    measurementId: "G-MQDYKD7YY8"
-}
-
 // export default {
-//     apiKey: "AIzaSyDDBXA5leJ9fSh4xDrIOEQsE_Cuk_U9bho",
-//     authDomain: "ministrants-production.firebaseapp.com",
-//     databaseURL: "https://ministrants-production.firebaseio.com",
-//     projectId: "ministrants-production",
-//     storageBucket: "ministrants-production.appspot.com",
-//     messagingSenderId: "1060998977929",
-//     appId: "1:1060998977929:web:f708a90a2662f04ea4e958",
-//     measurementId: "G-9X9ZJTZ3NG"
-//   };
+//     apiKey: "AIzaSyDkgawjQv37q19tCMUcch9_76EjiRYsMok",
+//     authDomain: "ministrans-60ff9.firebaseapp.com",
+//     databaseURL: "https://ministrans-60ff9.firebaseio.com",
+//     projectId: "ministrans-60ff9",
+//     storageBucket: "ministrans-60ff9.appspot.com",
+//     messagingSenderId: "358919010104",
+//     appId: "1:358919010104:web:fd0c57f712301f910473cd",
+//     measurementId: "G-MQDYKD7YY8"
+// }
+
+export default {
+    apiKey: "AIzaSyDDBXA5leJ9fSh4xDrIOEQsE_Cuk_U9bho",
+    authDomain: "ministrants-production.firebaseapp.com",
+    databaseURL: "https://ministrants-production.firebaseio.com",
+    projectId: "ministrants-production",
+    storageBucket: "ministrants-production.appspot.com",
+    messagingSenderId: "1060998977929",
+    appId: "1:1060998977929:web:f708a90a2662f04ea4e958",
+    measurementId: "G-9X9ZJTZ3NG"
+  };
