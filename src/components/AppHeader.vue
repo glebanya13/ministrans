0acb969664032a2cebb3088157ef6a6a461f90c5
unknown <senddownrain@gmail.com>
2020-10-27 09:58:22 +0300

    fixe menu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,39 +1,35 @@
 <template>
-  <div> 
-    <v-navigation-drawer
-      absolute
-      temporary
-      v-model="drawer"
-    >
-    <v-layout column align-center v-if="isUserAuthenticated">
-          <v-flex class="mt-5">
-            <v-avatar size="100">
-              <img  v-if="!userImage" src="../assets/user.png" alt="">
-              <v-img v-else :src="userImage"></v-img>
-            </v-avatar>
-            <p class="subheading mt-1 text-center">
-              {{userName}}
-            </p>
-          </v-flex>
+  <div>
+    <v-navigation-drawer fixed temporary v-model="drawer">
+      <v-layout column align-center v-if="isUserAuthenticated">
+        <v-flex class="mt-5">
+          <v-avatar size="100">
+            <img v-if="!userImage" src="../assets/user.png" alt="" />
+            <v-img v-else :src="userImage"></v-img>
+          </v-avatar>
+          <p class="subheading mt-1 text-center">
+            {{ userName }}
+          </p>
+        </v-flex>
       </v-layout>
       <v-list>
-
-<v-list-group
+        <v-list-group
           v-if="isAdmin && isUserAuthenticated"
           :value="false"
           no-action
           prepend-icon="mdi-account-circle"
         >
-        <template v-slot:activator>
+          <template v-slot:activator>
             <v-list-item-content>
               <v-list-item-title>Admin</v-list-item-title>
             </v-list-item-content>
           </template>
 
-        <v-list-item
+          <v-list-item
             v-for="link in admins"
             :key="link.title"
-            router :to="link.route"
+            router
+            :to="link.route"
           >
             <v-list-item-title>{{ link.title }}</v-list-item-title>
 
@@ -42,7 +38,6 @@
             </v-list-item-icon>
           </v-list-item>
         </v-list-group>
-
 
         <v-list-item
           v-for="link in users"
@@ -58,24 +53,23 @@
           </v-list-item-content>
         </v-list-item>
 
-        
-
         <v-list-group
           v-if="isUserAuthenticated"
           :value="false"
           no-action
           prepend-icon="mdi-pencil"
         >
-        <template v-slot:activator>
+          <template v-slot:activator>
             <v-list-item-content>
               <v-list-item-title>Отметка</v-list-item-title>
             </v-list-item-content>
           </template>
 
-        <v-list-item
+          <v-list-item
             v-for="link in marks"
             :key="link.title"
-            router :to="link.route"
+            router
+            :to="link.route"
           >
             <v-list-item-title>{{ link.title }}</v-list-item-title>
 
@@ -84,7 +78,6 @@
             </v-list-item-icon>
           </v-list-item>
         </v-list-group>
-
 
         <v-list-item text style="cursor: pointer" v-if="isUserAuthenticated">
           <v-list-item-icon>
@@ -99,11 +92,9 @@
       </v-list>
     </v-navigation-drawer>
     <v-app-bar app color="primary" dark>
-      <v-app-bar-nav-icon
-        @click.stop="drawer = !drawer"
-      ></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <router-link :to="{path: homeLink}" tag="span" style="cursor: pointer">
+      <router-link :to="{ path: homeLink }" tag="span" style="cursor: pointer">
         <v-toolbar-title>Ministrants.site</v-toolbar-title>
       </router-link>
 
@@ -142,35 +133,63 @@ export default {
     isSenior() {
       return this.$store.getters.isSenior;
     },
-    users(){
+    users() {
       let links = [
-        { icon: 'face', title: 'Регистрация', route: '/signup'},  
-        { icon: 'mdi-lock-open', title: 'Вход', route: '/signin'}
-      ]
-      if(this.isUserAuthenticated)
-      links = [
-        { icon: "mdi-account", title: "Профиль", route: "/profile" },
-        { icon: "mdi-account-multiple", title: "Список министрантов", route: "/ministrans" }
-      ]
-      return links
+        { icon: "face", title: "Регистрация", route: "/signup" },
+        { icon: "mdi-lock-open", title: "Вход", route: "/signin" },
+      ];
+      if (this.isUserAuthenticated)
+        links = [
+          { icon: "mdi-account", title: "Профиль", route: "/profile" },
+          {
+            icon: "mdi-account-multiple",
+            title: "Список министрантов",
+            route: "/ministrans",
+          },
+        ];
+      return links;
     },
-    admins(){
+    admins() {
       let links = [
-        { icon: "mdi-text-box-multiple-outline", title: 'AddNews', route: '/addnews' },
-        { icon: "mdi-text-box-multiple-outline", title: "Admin", route: "/admin" },
-        { icon: "mdi-text-box-multiple-outline", title: "Migration", route: "/migration" },
-        { icon: "mdi-text-box-multiple-outline", title: "Parish", route: "/parish" }
-      ]
-      return links
+        {
+          icon: "mdi-text-box-multiple-outline",
+          title: "AddNews",
+          route: "/addnews",
+        },
+        {
+          icon: "mdi-text-box-multiple-outline",
+          title: "Admin",
+          route: "/admin",
+        },
+        {
+          icon: "mdi-text-box-multiple-outline",
+          title: "Migration",
+          route: "/migration",
+        },
+        {
+          icon: "mdi-text-box-multiple-outline",
+          title: "Parish",
+          route: "/parish",
+        },
+      ];
+      return links;
     },
     marks() {
       let links = [
-        { icon: "mdi-text-box-multiple-outline", title: "Расписание", route: "/timetable" },
-        { icon: "mdi-text-box-multiple-outline", title: "Кто сегодня?", route: "/schedule" },
+        {
+          icon: "mdi-text-box-multiple-outline",
+          title: "Расписание",
+          route: "/timetable",
+        },
+        {
+          icon: "mdi-text-box-multiple-outline",
+          title: "Кто сегодня?",
+          route: "/schedule",
+        },
         { icon: "mdi-list-status", title: "Посещение", route: "/stats" },
-        { icon: "mdi-check-decagram", title: "Отметиться", route: "/checkin" }
-      ]
-      return links
+        { icon: "mdi-check-decagram", title: "Отметиться", route: "/checkin" },
+      ];
+      return links;
     },
     userImage() {
       let image = this.$store.getters.url;
@@ -179,8 +198,8 @@ export default {
     userName() {
       let name = this.$store.getters.userName;
       let surname = this.$store.getters.userSurname;
-      let full = name + " " + surname
-      return full
+      let full = name + " " + surname;
+      return full;
     },
   },
   methods: {
@@ -198,5 +217,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
